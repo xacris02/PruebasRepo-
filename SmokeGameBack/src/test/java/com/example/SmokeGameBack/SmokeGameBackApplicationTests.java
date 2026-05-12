4d842db70b221dc57d9f0f98443f07954a43fbde
Zorrochile15 <ju.elguetas@duocuc.cl>
2026-05-12 18:07:45 -0400

    pipeline 6

--- SmokeGameBack/src/test/java/com/example/SmokeGameBack/SmokeGameBackApplicationTests.java
+++ SmokeGameBack/src/test/java/com/example/SmokeGameBack/SmokeGameBackApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.SmokeGameBack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
 class SmokeGameBackApplicationTests {
 
 	@Test
