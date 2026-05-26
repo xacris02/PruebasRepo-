a498879932964bf904622692be5fe1c11adb92e0
xacris02 <al.chirinoslitano@duocuc.cl>
2026-05-26 17:03:12 -0400

    PruebaBuildAPIV2

--- SmokeGameBack/src/main/java/com/example/SmokeGameBack/config/SwaggerConfig.java
+++ SmokeGameBack/src/main/java/com/example/SmokeGameBack/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("SmokeGames API")
+                        .title("SmokeGames APIV2")
                         .version("1.0")
                         .description("Documentación del Backend de SmokeGames"));
     }
