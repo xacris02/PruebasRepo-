fd112ffc7304dbee2eabae54c63ec7fe12dfeb58
xacris02 <al.chirinoslitano@duocuc.cl>
2026-06-28 21:03:38 -0400

    Delete SmokeGameBack directory

--- SmokeGameBack/src/main/java/com/example/SmokeGameBack/repository/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SmokeGameBack.repository;
-
-import com.example.SmokeGameBack.model.Contacto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactoRepository extends JpaRepository<Contacto, Long> {
-}
--- SmokeGameBack/src/main/java/com/example/SmokeGameBack/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SmokeGameBack.repository;
-
-import com.example.SmokeGameBack.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    
-}
