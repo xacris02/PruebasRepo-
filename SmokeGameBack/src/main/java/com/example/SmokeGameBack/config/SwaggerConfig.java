9234bfc1264326de66df3ecc4694e7e790a07b2a
xacris02 <al.chirinoslitano@duocuc.cl>
2026-06-30 17:39:47 -0400

    cambio de v7 a v8
    en swagger config

--- SmokeGameBack/src/main/java/com/example/SmokeGameBack/config/SwaggerConfig.java
+++ SmokeGameBack/src/main/java/com/example/SmokeGameBack/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("SmokeGames APIV7")
+                        .title("SmokeGames APIV8")
                         .version("1.0")
                         .description("Documentación del Backend de SmokeGames"));
     }
